ed6bf63d521412349daa3d7bc0b07a78b161aa81
_endrilickollari <endri.lickollari@ltadvisors.it>
2025-04-07 13:23:05 +0200

    add fallback url for base api

--- src/components/services/api.jsx
+++ src/components/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://cerberus-api-0773eaec6d0f.herokuapp.com';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
